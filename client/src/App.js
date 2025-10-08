521e54f47b181698cb57293711c10300dc6fbac9
oliviaraisbeck <oeraisbeck@wpi.edu>
2025-10-08 13:08:05 -0400

    Added axios and cors, api call

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const apiCall = () =>{
+    axios.get('http://localhost:8080').then(() => {
+      console.log('working')
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={apiCall}>Make API Call</button>
+        
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello its a page')
 })
 
-app.listen(3000, () => {
-      console.log('server listening on port 3000')
+app.listen(8080, () => {
+      console.log('server listening on port 8080')
 })
