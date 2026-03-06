e9c737dc956221e3534d877e96c5eeb86468fe58
oliviaraisbeck <oeraisbeck@wpi.edu>
2026-03-06 16:49:19 -0500

    homepage content complete and mission banner cssing

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -10,7 +10,7 @@ const importAllAnimals = () => {
 const animals = importAllAnimals();
 
 const shuffleArray = (array) => {
-  const arr = [...array]; // copy array so original is not mutated
+  const arr = [...array]; 
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [arr[i], arr[j]] = [arr[j], arr[i]];
@@ -20,16 +20,19 @@ const shuffleArray = (array) => {
 
 const HomePage = () => {
   const navigate = useNavigate();
-  const bannerImage = "/images/tiger.jpg";
+  const bannerTiger = "/images/tiger.jpg";
+  const bannerButterfly = "/images/migratory_monarch_butterfly.jpg"
+  const donationLink = "https://support.nwf.org/page/89607/donate/1?ea.tracking.id=SRC&gclsrc=aw.ds&gad_source=1&gad_campaignid=23266753855&gbraid=0AAAAAD_rpHUnqMvZaNXTrvaOEU9V38Exe&gclid=CjwKCAiA8vXIBhAtEiwAf3B-gyjH2z63O97sbyanZx3g7-rT2EWko6wHTpw9cjWNqKSnDCeBu707DxoCOfsQAvD_BwE"
+  
   const goToAnimalPage = (name) => {
     navigate(`/animals/${name.replace(/\s+/g, "_")}`);
   };
   const shuffledAnimals = shuffleArray(animals);
 
   return (
-    <div className="container">
+    <div className="home-container">
       <div className="banner-home">
-        <img src={bannerImage} alt="Banner" className="banner-image" />
+        <img src={bannerTiger} alt="Banner" className="banner-image" />
         <div className="banner-text">
           <div className="stats">
             <div className="stat-line">
@@ -70,8 +73,33 @@ const HomePage = () => {
             ))}
           </div>
       </div>
-      <div className="mission">
-
+      <div className="mission-banner">
+        <img src={bannerButterfly} alt="Banner" className="banner-image" />
+        <div className="mission-text">
+          <div className="stats">
+            <div className="stat-line">
+              <h1>Our Mission</h1>
+            </div>
+            <div className="mission-line">
+              <h3>We pair individuals with an endangered species to inspire them to guarantee their animal’s survival.</h3>
+            </div>
+          </div>
+          <div className="mission-buttons">
+            <button onClick={() => navigate('/mission')}>
+              Read More
+            </button>
+            <button className="button1" onClick={() => { //set as national wildlife organization idk if we want a different one? 
+            window.open(donationLink, "_blank", "noopener,noreferrer");}}>
+              HELP NOW!
+            </button> 
+          </div>
+        </div>
+      </div>
+      <div className="partner-org">
+        <h2>Partner Organizations</h2>
+      </div>
+      <div className="lower-banner">
+        <h2>Work Cited Stuff</h2>
       </div>
     </div>
   );
