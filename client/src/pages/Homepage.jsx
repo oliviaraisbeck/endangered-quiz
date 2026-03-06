bc8470c6ed71ecf48b21cc72dd09f1dbf04af150
oliviaraisbeck <oeraisbeck@wpi.edu>
2026-03-06 15:27:49 -0500

    about homepage section

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/Homepage.css';
-import bannerImage from '../data/tiger.jpg';
+ //'../data/tiger.jpg';
 
 const HomePage = () => {
   const navigate = useNavigate();
+  const bannerImage = "/images/tiger.jpg";
 
   return (
     <div className="container">
@@ -22,14 +23,25 @@ const HomePage = () => {
               <h1>7 Minutes</h1>
             </div>
           </div>
-
           <button onClick={() => navigate('/quiz')}>
             Which Endangered <br /> Animal Are You
           </button>
         </div>
       </div>
-      <h1>About Your Animal Ally</h1>
-      <p>The Animal Ally Quiz matches your unique traits with one of 32 endangered animals. Take the quiz to find out more about yourself, and how you can make a difference for your critter.  </p>
+      <div className="about">
+        <h2>About Your Animal Ally</h2>
+        <p>The Animal Ally Quiz matches your unique traits with one of 32 endangered animals. Take the quiz to find out more about yourself, and how you can make a difference for your critter.  </p>
+        <button className="button1" onClick={() => navigate('/quiz')}>Take The Quiz</button>
+        <p className="learn-more" onClick={() => navigate("/about") } >
+          Learn More →
+        </p>
+      </div>
+      <div className="our-animals">
+        <h2>Our Animals</h2>
+        
+
+      </div>
+      
     </div>
   );
 };
