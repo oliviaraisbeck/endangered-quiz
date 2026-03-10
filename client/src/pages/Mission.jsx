f4d02dedd1ddafaf224cc7fc98c13a2dbec1b2d0
oliviaraisbeck <oeraisbeck@wpi.edu>
2026-03-10 18:11:48 -0400

    Some mission content in, quiz a bit bigger

--- client/src/pages/Mission.jsx
+++ client/src/pages/Mission.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/Mission.css';
+import text from '../data/mission.json';
 
 const Mission = () => {
     return (
       <div className="container">
-        <h1 className="title">Mission</h1>
+        <div className="content-mission">
+          <h1 className="title">Mission</h1>
+          <p>{ text.description}</p>
+        </div>
+        <div className="banner">
+          { text.banner1 }
+        </div>
+        <div className="content-mission">
+          <p>{ text.description2}</p>
+        </div>
       </div>
     );
   };
